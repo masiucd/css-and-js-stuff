d7d43698dff62b9a2f1923be4f87c63bc3929796
Masiu <ciszekmarcell@gmail.com>
2020-05-06 10:03:49 +0200

    add

--- generics/index.ts
+++ generics/index.ts
@@ -4,4 +4,10 @@ function identity<T>(arg: T): T {
 
 const x = identity<string>('apa');
 const xs = identity<number>(2);
-console.log(x, xs);
+
+const giveMeLast = <T>(arr: Array<T>): T => arr[arr.length - 1];
+
+const l1 = giveMeLast<number>([1, 2, 3, 4]);
+const l2 = giveMeLast<string>(['jon', 'tina', 'monkey', 'kiwi']);
+
+console.log(l1, l2);
