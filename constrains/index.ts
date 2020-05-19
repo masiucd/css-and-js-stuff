83ef8dbeae8ecf57fc1947d840a6f26253036aa4
Masiu <ciszekmarcell@gmail.com>
2020-05-19 10:48:55 +0200

    add

---
+++ constrains/index.ts
@@ -0,0 +1,21 @@
+interface Dog {
+  name: string;
+}
+interface Container<T> {
+  unit: T;
+}
+
+interface Bird extends Dog {
+  color: string;
+}
+
+const birdy: Bird = {
+  name: 'Birdy',
+  color: 'green',
+};
+
+const myContainer: Container<number> = {
+  unit: 22,
+};
+
+console.log(myContainer);
