3113ac2ce6f7e1cd8a57d69eace4cf41e3cf1c1b
masiucd <ciszekmarcell@gmail.com>
2024-02-25 09:02:52 +0100

    Update tailwind.config.ts with color changes and plugin addition

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 const config: Config = {
   content: [
@@ -8,13 +9,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: colors.cyan,
+        gray: colors.slate,
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
 export default config;
