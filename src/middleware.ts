8972c744b7eba381873f61a56b9810aba95a2dfa
masiucd <ciszekmarcell@gmail.com>
2024-03-23 15:07:16 +0700

    Add middleware for session management

---
+++ src/middleware.ts
@@ -0,0 +1,7 @@
+import {type NextRequest} from "next/server";
+
+import {updateSession} from "./lib/session";
+
+export async function middleware(request: NextRequest) {
+  return await updateSession(request);
+}
