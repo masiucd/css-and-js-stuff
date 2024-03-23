c1cd9b1cfd7f37f7d9fb39bc15712fd511d628fd
masiucd <ciszekmarcell@gmail.com>
2024-03-23 15:07:23 +0700

    Add session update and destroy functions

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,6 +1,7 @@
 import "server-only";
 
 import {cookies} from "next/headers";
+import {type NextRequest, NextResponse} from "next/server";
 
 import {decrypt, encrypt} from "./jwt";
 
@@ -21,11 +22,31 @@ export async function getSession() {
   return await decrypt(session);
 }
 
+export async function destroySession() {
+  cookies().delete(sessionName);
+}
+
+export async function updateSession(request: NextRequest) {
+  let session = request.cookies.get("session");
+  if (!session) return;
+
+  // Refresh session so it doesn't expire
+  let parsed = await decrypt(session.value);
+  parsed.expires = getExpires();
+
+  let res = NextResponse.next();
+  res.cookies.set({
+    name: "session",
+    value: await encrypt({
+      email: parsed.email as string,
+      expires: parsed.expires as Date,
+    }),
+    httpOnly: true, // only readable by the server
+    expires: parsed.expires as Date,
+  });
+}
+
 function getExpires(seconds: number = 3600): Date {
   let milliseconds = 1000;
   return new Date(Date.now() + seconds * milliseconds); // 1 hour
-}
-
-export async function destroySession() {
-  cookies().delete(sessionName);
 }
