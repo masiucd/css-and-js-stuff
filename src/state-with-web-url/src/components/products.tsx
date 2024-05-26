74008995b4d4e85a84e995fe6d20dc98717288e1
masiucd <ciszekmarcell@gmail.com>
2024-05-26 14:04:33 +0200

    refactor: Remove commented out code and console logs from Products component

--- src/state-with-web-url/src/components/products.tsx
+++ src/state-with-web-url/src/components/products.tsx
@@ -54,11 +54,6 @@ export function Products({
               product.id === 1 ? "pointer-events-none" : "pointer-events-auto"
             )}
             href={`/?id=${product.id - 1}&name=${slugify(product.name)}&size=xl`}
-            // onClick={() => {
-            //   setCurrentProductIndex((prev) =>
-            //     prev === 0 ? products.length - 1 : prev - 1
-            //   );
-            // }}
           >
             <span>
               <Icons.ArrowLeft />
@@ -82,11 +77,6 @@ export function Products({
                 : "pointer-events-auto"
             )}
             href={`/?id=${product.id + 1}&name=${slugify(product.name)}&size=${paramSize}`}
-            // onClick={() => {
-            //   setCurrentProductIndex((prev) =>
-            //     prev === products.length - 1 ? 0 : prev + 1
-            //   );
-            // }}
           >
             <span>
               <Icons.ArrowRight />
@@ -94,7 +84,7 @@ export function Products({
           </Link>
         </div>
         <ul className="flex gap-2">
-          {sizes.map((size) => {
+          {product.amiableSizes.map(({size, available}) => {
             let selectedSize = paramsId === product.id && size === paramSize;
             let searchParamsURL = new URLSearchParams(params.toString());
             searchParamsURL.set("size", size);
@@ -108,7 +98,7 @@ export function Products({
                 key={size}
                 className={cn(
                   "min-w-12 cursor-pointer rounded-md uppercase bg-main-950 p-2 text-center font-bold  text-main-50 shadow",
-                  product.amiableSizes.includes(size)
+                  available
                     ? "opacity-100 pointer-events-auto"
                     : "opacity-50 pointer-events-none",
                   selectedSize && "bg-main-50 text-main-950"
@@ -128,34 +118,134 @@ export function Products({
   );
 }
 
-let sizes = Object.freeze(["xs", "sm", "md", "lg", "xl", "2xl"]);
+// let sizes = Object.freeze(["xs", "sm", "md", "lg", "xl", "2xl"]);
 let products = Object.freeze([
   {
     id: 1,
     name: "Black shirt Uno",
     imageUrl: "/shirt-black.jpg",
-    amiableSizes: sizes.filter((_, i) => isEven(i)),
+    amiableSizes: [
+      {
+        size: "xs",
+        available: true,
+      },
+      {
+        size: "sm",
+        available: true,
+      },
+      {
+        size: "md",
+        available: true,
+      },
+      {
+        size: "lg",
+        available: true,
+      },
+      {
+        size: "xl",
+        available: true,
+      },
+      {
+        size: "2xl",
+        available: false,
+      },
+    ],
     price: 100.25,
   },
   {
     id: 2,
     name: "White shirt",
     imageUrl: "/shirt-white.jpg",
-    amiableSizes: sizes.filter((_, i) => isOdd(i)),
+    amiableSizes: [
+      {
+        size: "xs",
+        available: true,
+      },
+      {
+        size: "sm",
+        available: true,
+      },
+      {
+        size: "md",
+        available: true,
+      },
+      {
+        size: "lg",
+        available: true,
+      },
+      {
+        size: "xl",
+        available: true,
+      },
+      {
+        size: "2xl",
+        available: true,
+      },
+    ],
     price: 200.0,
   },
   {
     id: 3,
     name: "Black shirt Dos",
     imageUrl: "/shirt-black-two.jpg",
-    amiableSizes: sizes.filter((_, i) => isDivisibleBy(i, 3)),
+    amiableSizes: [
+      {
+        size: "xs",
+        available: true,
+      },
+      {
+        size: "sm",
+        available: true,
+      },
+      {
+        size: "md",
+        available: true,
+      },
+      {
+        size: "lg",
+        available: true,
+      },
+      {
+        size: "xl",
+        available: true,
+      },
+      {
+        size: "2xl",
+        available: true,
+      },
+    ],
     price: 120.55,
   },
   {
     id: 4,
     name: "Cat shirt",
     imageUrl: "/shirt-cat.jpg",
-    amiableSizes: sizes.filter((_, i) => isDivisibleBy(i, 2)),
+    amiableSizes: [
+      {
+        size: "xs",
+        available: true,
+      },
+      {
+        size: "sm",
+        available: true,
+      },
+      {
+        size: "md",
+        available: true,
+      },
+      {
+        size: "lg",
+        available: true,
+      },
+      {
+        size: "xl",
+        available: true,
+      },
+      {
+        size: "2xl",
+        available: true,
+      },
+    ],
     price: 150.75,
   },
 ]);
